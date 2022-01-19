1bbef13dc8321ce300f13854583604340086557c
wcp1231 <2058684+wcp1231@users.noreply.github.com>
2022-01-19 15:20:30 +0800

    feature: add redis dissector

--- pkg/dissector/processor.go
+++ pkg/dissector/processor.go
@@ -2,6 +2,7 @@ package dissector
 
 import (
 	"cocoon/pkg/dissector/http"
+	"cocoon/pkg/dissector/redis"
 	"cocoon/pkg/model/api"
 )
 
@@ -26,18 +27,9 @@ func (d *DissectProcessor) Process(b api.TcpReader) {
 	}
 }
 
-func (d *DissectProcessor) findMatchedDissector(b api.TcpReader) api.Dissector {
-	for _, dissector := range d.dissectors {
-		if dissector.Match(b) {
-			return dissector
-		}
-	}
-	// 返回默认处理器
-	return d.dissectors[len(d.dissectors)-1]
-}
-
 func NewDissectProcessor(tcpId string, isRequest bool, resultC chan *api.DissectResult) *DissectProcessor {
 	var dissectors []api.Dissector
+	dissectors = append(dissectors, redis.NewDissector(resultC))
 	dissectors = append(dissectors, http.NewDissector(resultC))
 	dissectors = append(dissectors, newDefaultDissector(resultC))
 	return &DissectProcessor{
